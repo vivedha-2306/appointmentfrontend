3a732afc69fa1db209a9ef0d9326742a2b80ce88
vivedha-2306 <vivedha.a.2023.cse@ritchennai.edu.in>
2026-02-17 09:06:51 +0530

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
-const API_BASE = 'http://127.0.0.1:5000';
+const API_BASE = 'https://appointmentbackend-vebl.onrender.com';
 
 export default function Register() {
   const [form, setForm] = useState({ name: '', email: '', password: '' });
